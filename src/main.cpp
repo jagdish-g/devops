3d030d04361cb9cec82b4a84983e5b3aa8f14a88
jagdish-g <jagdishgm2019@gmail.com>
2025-03-06 11:12:35 -0500

    fix statis code filure

--- src/main.cpp
+++ src/main.cpp
@@ -3,8 +3,8 @@
 
 int main() {
 
-    int i,jk,*p;
-    i=*p;
+    //int i,jk,*p; to make static code analysis to fail 
+    //i=*p;
     std::cout << "2 + 3 = " << add(2, 3) << std::endl;
     std::cout << "2 / 0  = "  << division(2, 0) << std::endl;
     return 0;
